9fddaaae9c636c96bc24dce552cf45ea1cac7632
尚笑天 <tianshang359@gmail.com>
2023-11-07 18:10:41 +0800

    练习使用外部集成控件

--- app/src/main/java/com/jnu/student/MapFragment.java
+++ app/src/main/java/com/jnu/student/MapFragment.java
@@ -8,6 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.tencent.tencentmap.mapsdk.maps.CameraUpdateFactory;
+import com.tencent.tencentmap.mapsdk.maps.TencentMap;
+import com.tencent.tencentmap.mapsdk.maps.model.LatLng;
+import com.tencent.tencentmap.mapsdk.maps.model.Marker;
+import com.tencent.tencentmap.mapsdk.maps.model.MarkerOptions;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MapFragment#newInstance} factory method to
@@ -43,11 +49,52 @@ public class MapFragment extends Fragment {
         if (getArguments() != null) {
         }
     }
-
+    private com.tencent.tencentmap.mapsdk.maps.MapView mapView = null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = rootView.findViewById(R.id.mapView);
+
+        TencentMap tencentMap = mapView.getMap();
+
+        LatLng point1 = new LatLng(22.255453, 113.54145);
+        tencentMap.moveCamera(CameraUpdateFactory.newLatLng(point1));
+
+        // 创建一个Marker对象
+        MarkerOptions markerOptions = new MarkerOptions(point1)
+                .title("标记标题");
+
+        // 添加标记到地图上
+        Marker marker = tencentMap.addMarker(markerOptions);
+
+        return rootView;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
